e62d6f396cf9449cd989f4194b94176cef515c5c
Mudunuri Shrilekha <148220438+Shrilekha-369@users.noreply.github.com>
2025-11-02 16:23:26 +0530

    Update config.ts

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -2,7 +2,7 @@ export const firebaseConfig = {
   "projectId": "studio-3330850809-f833f",
   "appId": "1:1098159079759:web:535673702826c60471453a",
   "storageBucket": "studio-3330850809-f833f.appspot.com",
-  "apiKey": "AIzaSyCNic3DV9KedzpSpA3D13Bu-OIDNKLpHbw",
+  "apiKey": "API-KEY",
   "authDomain": "studio-3330850809-f833f.firebaseapp.com",
   "measurementId": "",
   "messagingSenderId": "1098159079759"
