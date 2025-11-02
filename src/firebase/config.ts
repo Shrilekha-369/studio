a60f582b21f0038fe99aed4a665b8931520308d0
Mudunuri Shrilekha <shrilekha.cse@gmail.com>
2025-11-02 06:38:21 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-3330850809-f833f",
+  "appId": "1:1098159079759:web:535673702826c60471453a",
+  "apiKey": "AIzaSyCNic3DV9KedzpSpA3D13Bu-OIDNKLpHbw",
+  "authDomain": "studio-3330850809-f833f.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "1098159079759"
+};
