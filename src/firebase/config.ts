107f3a1b58bdcac2093a2295bf244acaf8d78ab4
Mudunuri Shrilekha <shrilekha.cse@gmail.com>
2025-11-02 10:30:49 +0000

    this

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,6 +1,7 @@
 export const firebaseConfig = {
   "projectId": "studio-3330850809-f833f",
   "appId": "1:1098159079759:web:535673702826c60471453a",
+  "storageBucket": "studio-3330850809-f833f.appspot.com",
   "apiKey": "AIzaSyCNic3DV9KedzpSpA3D13Bu-OIDNKLpHbw",
   "authDomain": "studio-3330850809-f833f.firebaseapp.com",
   "measurementId": "",
