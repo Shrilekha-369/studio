c8f013714902c75262318f7456ebce3456135c24
Mudunuri Shrilekha <shrilekha.cse@gmail.com>
2025-11-02 06:18:16 +0000

    Try fixing this error: `Runtime Error: Error: Map container is already i

--- src/components/map-component.tsx
+++ src/components/map-component.tsx
@@ -3,6 +3,7 @@
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 import L from 'leaflet';
+import { useEffect, useRef } from 'react';
 
 // Fix for default icon path issue with webpack
 delete (L.Icon.Default.prototype as any)._getIconUrl;
@@ -15,10 +16,21 @@ L.Icon.Default.mergeOptions({
 
 
 export function MapComponent() {
+  const mapRef = useRef<HTMLDivElement>(null);
   const position: [number, number] = [18.4875, -67.1279]; // ONE Fitness Studio Aguadilla
 
+  useEffect(() => {
+    // This is a workaround for a bug in react-leaflet in React 18 strict mode
+    // that causes the map to be initialized twice.
+    if (mapRef.current && (mapRef.current as any)._leaflet_id) {
+      return;
+    }
+  }, []);
+
   return (
-      <MapContainer center={position} zoom={15} scrollWheelZoom={false} className="h-full w-full rounded-lg">
+      <MapContainer whenReady={() => {
+          if (mapRef.current) (mapRef.current as any)._leaflet_id = true;
+      }} center={position} zoom={15} scrollWheelZoom={false} className="h-full w-full rounded-lg">
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
