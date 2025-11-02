1ce96453f811634e7b126f26a796ad328951c5dd
Mudunuri Shrilekha <shrilekha.cse@gmail.com>
2025-11-02 12:38:27 +0000

    The user details are not visible in the manage bookings section of the d

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -2,7 +2,7 @@ export const firebaseConfig = {
   "projectId": "studio-3330850809-f833f",
   "appId": "1:1098159079759:web:535673702826c60471453a",
   "storageBucket": "studio-3330850809-f833f.appspot.com",
-  "apiKey": "API-KEY",
+  "apiKey": "AIzaSyCNic3DV9KedzpSpA3D13Bu-OIDNKLpHbw",
   "authDomain": "studio-3330850809-f833f.firebaseapp.com",
   "measurementId": "",
   "messagingSenderId": "1098159079759"
